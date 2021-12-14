ca7ecf169e0d358f4a7af33f0d242bde56c9264b
AmnesiackY <hitsac2711@gmail.com>
2021-12-14 22:30:43 +0200

    add PRAVKI ver 2.0

--- Sharp_Encrypting/Sharp_Encrypting/Program.cs
+++ Sharp_Encrypting/Sharp_Encrypting/Program.cs
@@ -8,35 +8,7 @@ namespace Sharp_Encrypting
         static void Main(string[] args)
         {
             int saturation_counter = 0;
-            Dictionary<int, char> lat_alpha = new Dictionary<int, char>
-            {
-                { 0, 'a' },
-                { 1, 'b' },
-                { 2, 'c' },
-                { 3, 'd' },
-                { 4, 'e' },
-                { 5, 'f' },
-                { 6, 'g' },
-                { 7, 'h' },
-                { 8, 'i' },
-                { 9, 'j' },
-                { 10, 'k' },
-                { 11, 'l' },
-                { 12, 'm' },
-                { 13, 'n' },
-                { 14, 'o' },
-                { 15, 'p' },
-                { 16, 'q' },
-                { 17, 'r' },
-                { 18, 's' },
-                { 19, 't' },
-                { 20, 'u' },
-                { 21, 'v' },
-                { 22, 'w' },
-                { 23, 'x' },
-                { 24, 'y' },
-                { 25, 'z' }
-            };
+            
 
             Console.WriteLine("Hello young scrambler!");
             Console.WriteLine("Enter phrase for encrypting: ");
@@ -59,9 +31,9 @@ namespace Sharp_Encrypting
             {
                 key = key.Substring(0, input.Length);
             } 
-            string check_res = Transformations.Encr_vigener(input,key,lat_alpha);
+            string check_res = Transformations.Encr_vigener(input,key);
             Console.WriteLine($"\nEncryptet messsage - {check_res}");
-            Console.WriteLine($"\nDecryptet message - {Transformations.Decr_vigener(check_res,key,lat_alpha)}");
+            Console.WriteLine($"\nDecryptet message - {Transformations.Decr_vigener(check_res,key)}");
         }       
     }
 }
--- Sharp_Encrypting/Sharp_Encrypting/Transformations.cs
+++ Sharp_Encrypting/Sharp_Encrypting/Transformations.cs
@@ -6,55 +6,90 @@ namespace Sharp_Encrypting
 {
     class Transformations
     {
-        public static string Encr_vigener(string input, string key, Dictionary<int, char> lat_alpha)
+        static Dictionary<int, char> lat_betta = new Dictionary<int, char>
+            {
+                { 0, 'a' },
+                { 1, 'b' },
+                { 2, 'c' },
+                { 3, 'd' },
+                { 4, 'e' },
+                { 5, 'f' },
+                { 6, 'g' },
+                { 7, 'h' },
+                { 8, 'i' },
+                { 9, 'j' },
+                { 10, 'k' },
+                { 11, 'l' },
+                { 12, 'm' },
+                { 13, 'n' },
+                { 14, 'o' },
+                { 15, 'p' },
+                { 16, 'q' },
+                { 17, 'r' },
+                { 18, 's' },
+                { 19, 't' },
+                { 20, 'u' },
+                { 21, 'v' },
+                { 22, 'w' },
+                { 23, 'x' },
+                { 24, 'y' },
+                { 25, 'z' }
+            };
+        static Dictionary<char, int> lat_alpha = new Dictionary<char, int>
+            {
+                {'a', 0 },
+                {'b', 1 },
+                {'c', 2 },
+                {'d', 3 },
+                {'e', 4 },
+                {'f', 5 },
+                {'g', 6 },
+                {'h', 7 },
+                {'i', 8 },
+                {'j', 9 },
+                {'k', 10 },
+                {'l', 11 },
+                {'m', 12 },
+                {'n', 13 },
+                {'o', 14 },
+                {'p', 15 },
+                {'q', 16 },
+                {'r', 17 },
+                {'s', 18 },
+                {'t', 19 },
+                {'u', 20 },
+                {'v', 21 },
+                {'w', 22 },
+                {'x', 23 },
+                {'y', 24 },
+                {'z', 25 },
+            };
+        public static string Encr_vigener(string input, string key)
         {
             string result_encr = "";
             for (int i = 0; i < input.Length; i++)
             {
-                int encr_key = 0;
-                foreach (KeyValuePair<int, char> keyvalue in lat_alpha)
-                {
-                    if (input[i] == keyvalue.Value)
-                    {
-                        encr_key += keyvalue.Key;
-                    }
-                    if (key[i] == keyvalue.Value)
-                    {
-                        encr_key += keyvalue.Key;
-                    }
-                }
+                int encr_key = lat_alpha[input[i]] + lat_alpha[key[i]];
+
                 if (encr_key > 25)
                 {
                     encr_key -= 26;
                 }
-                result_encr += lat_alpha[encr_key];
+                result_encr += lat_betta[encr_key];
             }
             return result_encr;
         }
-        public static string Decr_vigener(string input, string key, Dictionary<int, char> lat_alpha)
+        public static string Decr_vigener(string input, string key)
         {
             string result = "";
             for (int i = 0; i < input.Length; i++)
-            {
-                int encoded_key = 0;
-                int keyword = 0;
-                foreach (KeyValuePair<int, char> keyvalue in lat_alpha)
-                {
-                    if (input[i] == keyvalue.Value)
-                    {
-                        encoded_key = keyvalue.Key;
-                    }
-                    if (key[i] == keyvalue.Value)
-                    {
-                        keyword = keyvalue.Key;
-                    }
-                }
-                int decrypyt_formulas = (encoded_key - keyword) + 26;
+            {                
+                int decrypyt_formulas = (lat_alpha[input[i]] - lat_alpha[key[i]]) + 26;
                 if (decrypyt_formulas > 25)
                 {
                     decrypyt_formulas -= 26;
                 }
-                result += lat_alpha[decrypyt_formulas];
+                result += lat_betta[decrypyt_formulas];
             }
             return result;
         }
