8143e6c241d5a8dd8105af254a8e73c5fd385ff4
AmnesiackY <hitsac2711@gmail.com>
2021-12-14 21:16:35 +0200

    adding a file/style code

--- Sharp_Encrypting/Sharp_Encrypting/Program.cs
+++ Sharp_Encrypting/Sharp_Encrypting/Program.cs
@@ -59,65 +59,9 @@ namespace Sharp_Encrypting
             {
                 key = key.Substring(0, input.Length);
             } 
-            string check_res = Encr_vigener(input,key,lat_alpha);
+            string check_res = Transformations.Encr_vigener(input,key,lat_alpha);
             Console.WriteLine($"\nEncryptet messsage - {check_res}");
-            Console.WriteLine($"\nDecryptet message - {Decr_vigener(check_res,key,lat_alpha)}");
-
-
-
-
-        }
-        static string Encr_vigener(string input,string key, Dictionary<int,char> lat_alpha)
-        {
-            string result_encr = "";
-            for(int i = 0; i < input.Length; i++)
-            {   
-                int encr_key = 0;   
-                foreach(KeyValuePair<int,char> keyvalue in lat_alpha)
-                {
-                    if(input[i] == keyvalue.Value)
-                    {
-                        encr_key += keyvalue.Key;
-                    }
-                    if(key[i] == keyvalue.Value)
-                    {
-                        encr_key += keyvalue.Key;
-                    }
-                }
-                if(encr_key > 25)
-                {
-                    encr_key -= 26;
-                }
-                result_encr += lat_alpha[encr_key];
-            }
-            return result_encr;
-        }
-        static string Decr_vigener(string input, string key, Dictionary<int, char> lat_alpha)
-        {
-            string result = "";
-            for (int i = 0; i < input.Length; i++)
-            {
-                int encoded_key = 0;
-                int keyword = 0;
-                foreach (KeyValuePair<int, char> keyvalue in lat_alpha)
-                {
-                    if (input[i] == keyvalue.Value)
-                    {
-                        encoded_key = keyvalue.Key;
-                    }
-                    if (key[i] == keyvalue.Value)
-                    {
-                        keyword = keyvalue.Key;
-                    }
-                }
-                int decrypyt_formulas = (encoded_key - keyword ) + 26;
-                if(decrypyt_formulas > 25)
-                {
-                    decrypyt_formulas -= 26;
-                }
-                result += lat_alpha[decrypyt_formulas];
-            }
-            return result;
-        }
+            Console.WriteLine($"\nDecryptet message - {Transformations.Decr_vigener(check_res,key,lat_alpha)}");
+        }       
     }
 }
---
+++ Sharp_Encrypting/Sharp_Encrypting/Transformations.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp_Encrypting
+{
+    class Transformations
+    {
+        public static string Encr_vigener(string input, string key, Dictionary<int, char> lat_alpha)
+        {
+            string result_encr = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                int encr_key = 0;
+                foreach (KeyValuePair<int, char> keyvalue in lat_alpha)
+                {
+                    if (input[i] == keyvalue.Value)
+                    {
+                        encr_key += keyvalue.Key;
+                    }
+                    if (key[i] == keyvalue.Value)
+                    {
+                        encr_key += keyvalue.Key;
+                    }
+                }
+                if (encr_key > 25)
+                {
+                    encr_key -= 26;
+                }
+                result_encr += lat_alpha[encr_key];
+            }
+            return result_encr;
+        }
+        public static string Decr_vigener(string input, string key, Dictionary<int, char> lat_alpha)
+        {
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                int encoded_key = 0;
+                int keyword = 0;
+                foreach (KeyValuePair<int, char> keyvalue in lat_alpha)
+                {
+                    if (input[i] == keyvalue.Value)
+                    {
+                        encoded_key = keyvalue.Key;
+                    }
+                    if (key[i] == keyvalue.Value)
+                    {
+                        keyword = keyvalue.Key;
+                    }
+                }
+                int decrypyt_formulas = (encoded_key - keyword) + 26;
+                if (decrypyt_formulas > 25)
+                {
+                    decrypyt_formulas -= 26;
+                }
+                result += lat_alpha[decrypyt_formulas];
+            }
+            return result;
+        }
+    }
+}
